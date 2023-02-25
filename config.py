677ce2caa3dbb662287267d295bc9f011a130b83
Saheel Acharya <saheel97@gmail.com>
2023-02-25 12:10:10 -0500

    Initial commit for a config file for the python projects in this repo

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+# Ariba configuration paths
+DOWNLOAD_DIRECTORY = 'C:\\Toronto-Bids-Downloads',
+REPO_DIRECTORY = 'C:\\\\code\\\\CivicTech\\\\toronto-bids\\\\scrapers',
+TORONTO_ARIBA_URL = 'https://service.ariba.com/Discovery.aw/ad/profile?key=AN01050912625#b0'
+SCRAPING_RETRY_INTERVAL_SEC = 600,
+OUTPUT_HTML_DIRECTORY = f'{REPO_DIRECTORY}'"
