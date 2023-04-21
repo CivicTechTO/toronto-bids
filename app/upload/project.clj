2f5bf851b40bf5fb0fedd37592fd2f52c7982e40
Jim Rootham <jrootham@gmail.com>
2023-04-21 16:02:33 -0400

    Stash changes hiding in usaved files

--- app/upload/project.clj
+++ app/upload/project.clj
@@ -1,9 +1,16 @@
 (defproject upload "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :description "Upload data for Toronto bids"
+  :dependencies 
+  [
+  	[org.clojure/clojure "1.10.0"]
+		[ring/ring-core "1.9.6"]
+		[ring/ring-jetty-adapter "1.9.6"]
+		[compojure "1.7.0"]
+		[org.clojure/data.json "2.4.0"]
+		[org.clojure/java.jdbc "0.7.12"]
+		[mysql/mysql-connector-java "8.0.32"]
+		[bananaoomarang/ring-debug-logging "1.1.0"]
+	]
   :main ^:skip-aot upload.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all :uberjar-name "upload.jar"}}
