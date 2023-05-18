90f7e61b35aef34da890bf667754deba61caf930
Clyne Sullivan <clyne@bitgloo.com>
2023-05-18 19:02:30 -0400

    style: date, color

--- app/server/tests/frontend/src/frontend/calls.clj
+++ app/server/tests/frontend/src/frontend/calls.clj
@@ -19,7 +19,7 @@
 (defn line1 [call]
 	[:div.datecall
 ;		[:div "<b>Posted:</b> " (format-date-time (get call "posting_date"))]
-		[:div "<b>Closed:</b> " (format-date-time (get call "closing_date"))]
+		[:div.date (format-date-time (get call "closing_date"))]
 		[:div.callno (get call "call_number")]
 	]
 )
