0a3a9468109f92898de57e6e38977e7b13ecac46
Saheel Acharya <saheel97@gmail.com>
2023-05-18 00:14:57 -0400

    Check to see if you've seen a document with the same callnumber and only insert if it's new

--- app/server/upload/upload-function/Function1.cs
+++ app/server/upload/upload-function/Function1.cs
@@ -11,6 +11,7 @@ using Newtonsoft.Json;
 using upload_function.DB;
 using System.Collections.Generic;
 using Type = upload_function.DB.OfferType;
+using System.Linq;
 
 namespace upload_function
 {
@@ -26,90 +27,91 @@ namespace upload_function
 
             try
             {
-
                 using var streamReader = new StreamReader("C:\\code\\CivicTech\\toronto-bids\\app\\server\\upload\\upload-function\\test-sample.json");
 
                 using var context = new BidsDBContext();
                 string body = streamReader.ReadToEnd();
-
                 dynamic data = JsonConvert.DeserializeObject(body);
 
-                var new_comm_type = new CommoditySubType
+                string callNumber = data.CallNumber;
+                var dbDoc = context.Documents.FirstOrDefault(x => x.CallNumber == callNumber);
+                if (dbDoc == null)
                 {
-                    SubTypeName = data.CommodityType,
-                    Commodity = new Commodity
+                    var new_comm_type = new CommoditySubType
                     {
-                        CommodityName = data.Commodity,
-                    }
-                };
-
-                var new_division = new Division
-                {
-                    Division1 = data.Division
-                };
-
-                var new_location = new Location
-                {
-                    Location1 = data.BuyerLocationShow
-                };
-
-                var new_buyer = new Buyer
-                {
-                    Buyer1 = data.ShowBuyerNameList,
-                    Phone = data.BuyerPhoneShow,
-                    Email = data.BuyerEmailShow,
-                    Location = new_location
-                };
-
-                var new_offer_type = new Type
-                {
-                    Type1 = data.Type
-                };
-
-
-                var new_document = new Document
-                {
-                    CallNumber = (data?.CallNumber == null ? "" : data?.CallNumber),
-                    ShortDescription = (data?.ShortDescription == null ? "" : data?.ShortDescription),
-                    Description = (data?.Description == null ? "" : data?.Description),
-                    PostingDate = DateOnly.MinValue,
-                    ClosingDate = DateOnly.MinValue,
-                    SearchText = (data?.SearchText == null ? "" : data?.SearchText),
-                    SiteMeeting = (data?.SiteMeeting == null ? "" : data?.SiteMeeting),
-                    LastUpdated = DateTime.Now,
-                    CommodityType = new_comm_type,
-                    Division = new_division,
-                    OfferType = new_offer_type,
-                    Attachments = new List<Attachment>(),
-                };
-
-                string[] fileNames = data.FileName.ToObject <string[]>();
-                string[] fileURIs = data.DownloadLink.ToObject<string[]>();
-                string[] filePaths = data.Location.ToObject<string[]>();
-
-                var minlength = Math.Min(fileNames.Length, Math.Min(fileURIs.Length, filePaths.Length));
-
-
-                for (int i = 0; i < minlength; i++)
-                {
-                    string uri = fileURIs[i];
-                    uri = uri.Replace(@"\/", "\\");
-
-                    string path = filePaths[i];
-                    path = path.Replace(@"\/", "\\");
-
-                    var attachment = new Attachment
-                    {
-                        FileName = fileNames[i],
-                        DatastoreFileURL = uri,
-                        AttachmentPath = path
+                        SubTypeName = data.CommodityType,
+                        Commodity = new Commodity
+                        {
+                            CommodityName = data.Commodity,
+                        }
                     };
 
-                    new_document.Attachments.Add(attachment);
+                    var new_division = new Division
+                    {
+                        Division1 = data.Division
+                    };
+
+                    var new_location = new Location
+                    {
+                        Location1 = data.BuyerLocationShow
+                    };
+
+                    var new_buyer = new Buyer
+                    {
+                        Buyer1 = data.ShowBuyerNameList,
+                        Phone = data.BuyerPhoneShow,
+                        Email = data.BuyerEmailShow,
+                        Location = new_location
+                    };
+
+                    var new_offer_type = new Type
+                    {
+                        Type1 = data.Type
+                    };
+
+                    var new_document = new Document
+                    {
+                        CallNumber = (data?.CallNumber == null ? "" : data?.CallNumber),
+                        ShortDescription = (data?.ShortDescription == null ? "" : data?.ShortDescription),
+                        Description = (data?.Description == null ? "" : data?.Description),
+                        PostingDate = DateOnly.MinValue,
+                        ClosingDate = DateOnly.MinValue,
+                        SearchText = (data?.SearchText == null ? "" : data?.SearchText),
+                        SiteMeeting = (data?.SiteMeeting == null ? "" : data?.SiteMeeting),
+                        LastUpdated = DateTime.Now,
+                        CommodityType = new_comm_type,
+                        Division = new_division,
+                        OfferType = new_offer_type,
+                        Attachments = new List<Attachment>(),
+                    };
+
+                    string[] fileNames = data.FileName.ToObject<string[]>();
+                    string[] fileURIs = data.DownloadLink.ToObject<string[]>();
+                    string[] filePaths = data.Location.ToObject<string[]>();
+
+                    var minlength = Math.Min(fileNames.Length, Math.Min(fileURIs.Length, filePaths.Length));
+
+                    for (int i = 0; i < minlength; i++)
+                    {
+                        string uri = fileURIs[i];
+                        uri = uri.Replace(@"\/", "\\");
+
+                        string path = filePaths[i];
+                        path = path.Replace(@"\/", "\\");
+
+                        var attachment = new Attachment
+                        {
+                            FileName = fileNames[i],
+                            DatastoreFileURL = uri,
+                            AttachmentPath = path
+                        };
+
+                        new_document.Attachments.Add(attachment);
+                    }
+
+                    context.Documents.Add(dbDoc);
+
                 }
-
-
-                context.Documents.Add(new_document);
 
                 context.SaveChanges();
 
