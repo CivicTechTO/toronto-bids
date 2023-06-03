757ab7c35a8cf8b34c4d04e289233700aa128b5b
Saheel Acharya <saheel97@gmail.com>
2023-06-02 20:55:34 -0400

    Document the flat-schema

--- app/db/flat-schema.sql
+++ app/db/flat-schema.sql
@@ -27,13 +27,13 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `attachments`
 --
-
+--PK (CallNumber,filename)
 CREATE TABLE `attachments` (
-  `CallNumber` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `filename` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
-  `parsedtext` longtext COLLATE utf8_unicode_ci,
-  `lastupdated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `uuid` varchar(36) COLLATE utf8_unicode_ci NOT NULL
+  `CallNumber` varchar(30) COLLATE utf8_unicode_ci NOT NULL, -- Reference to calls.CallNumber (TODO - MAKE THIS A FOREIGN KEY TO Calls table)
+  `filename` varchar(256) COLLATE utf8_unicode_ci NOT NULL, -- Name of the file 
+  `parsedtext` longtext COLLATE utf8_unicode_ci, -- (Please update)
+  `lastupdated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP -- Timestamp to track when the entry was inserted or otherwise last updated
+  `uuid` varchar(36) COLLATE utf8_unicode_ci NOT NULL 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -42,24 +42,25 @@ CREATE TABLE `attachments` (
 -- Table structure for table `calls`
 --
 
+--PK (CallNumber)
 CREATE TABLE `calls` (
-  `CallNumber` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `Commodity` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
-  `CommodityType` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
-  `Type` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
-  `ShortDescription` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
-  `Description` varchar(10000) COLLATE utf8_unicode_ci NOT NULL,
-  `ShowDatePosted` date NOT NULL,
-  `ClosingDate` date NOT NULL,
-  `SiteMeeting` varchar(1000) COLLATE utf8_unicode_ci NOT NULL,
-  `ShowBuyerNameList` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
-  `BuyerPhoneShow` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
-  `BuyerEmailShow` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
-  `Division` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
-  `BuyerLocationShow` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `CallNumber` varchar(30) COLLATE utf8_unicode_ci NOT NULL, -- Primary identifier used to differentiate individual calls/rfps/tenders
+  `Commodity` varchar(256) COLLATE utf8_unicode_ci NOT NULL, -- Classification for the given rfp that defines what department of the city that the call was placed from
+  `CommodityType` varchar(256) COLLATE utf8_unicode_ci NOT NULL, -- Sub classification for the given rfp that can vary based on the Commodity selected
+  `Type` varchar(256) COLLATE utf8_unicode_ci NOT NULL, -- Classification for the kind of call this is. We don't have an exhaustive list of what all values this field can take (We should mention where this field is sourced from in the scraped data)
+  `ShortDescription` varchar(256) COLLATE utf8_unicode_ci NOT NULL, -- Description of the rfp/tender offer/call taken from ariba
+  `Description` varchar(10000) COLLATE utf8_unicode_ci NOT NULL, -- Another field taken from the description of the rfp from ariba
+  `ShowDatePosted` date NOT NULL, -- The date that the call was posted
+  `ClosingDate` date NOT NULL, -- The date that the call is due to be closed. This can change
+  `SiteMeeting` varchar(1000) COLLATE utf8_unicode_ci NOT NULL, -- (Please update)
+  `ShowBuyerNameList` varchar(256) COLLATE utf8_unicode_ci NOT NULL, -- (Rename this field to be clearer) This field is used to store the name of the buyer from the city associated with the call
+  `BuyerPhoneShow` varchar(256) COLLATE utf8_unicode_ci NOT NULL, -- (Rename this field) Phone number associated with the buyer
+  `BuyerEmailShow` varchar(256) COLLATE utf8_unicode_ci NOT NULL, -- (Rename this field) Email associated with the buyer for this call
+  `Division` varchar(256) COLLATE utf8_unicode_ci NOT NULL, -- Another field to denote which City division the call is coming from
+  `BuyerLocationShow` varchar(256) COLLATE utf8_unicode_ci NOT NULL, -- (Rename this field) This is the location of the buyer associated with this call
   `parsedtext` longtext COLLATE utf8_unicode_ci,
-  `lastupdated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `uuid` varchar(36) COLLATE utf8_unicode_ci NOT NULL
+  `lastupdated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, --Timestamp field 
+  `uuid` varchar(36) COLLATE utf8_unicode_ci NOT NULL -- (Please update)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
